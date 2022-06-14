bc38c9d405891030b9edb2838eedacff670a36ec
Zach-Adcock <zwa2014@gmail.com>
2022-06-13 22:57:30 -0700

    vars reconfigured

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-require('dotenv').config()
+const dotenv = require('dotenv').config()
 const session = require("express-session")
 const MongoStore = require("connect-mongo");
 
